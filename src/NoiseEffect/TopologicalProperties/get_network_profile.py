93d239cff4b333c94f4dc38049907964a6684eb8
marlene-grabner <marlene.grabner@gmail.com>
2026-08-27 15:51:31 +0200

    fix(TopologicalProperties):changed sigma of eigenvalue solver to 1e-8 from 0

--- src/NoiseEffect/TopologicalProperties/get_network_profile.py
+++ src/NoiseEffect/TopologicalProperties/get_network_profile.py
@@ -68,7 +68,7 @@ def get_network_profile(G, metrics_on_gcc=True, verbose=False):
     log("Spectral properties...")
     L = nx.laplacian_matrix(H).astype(float)
     try:
-        eigvals = sla.eigsh(L, k=6, sigma=0, which="LM", return_eigenvectors=False)
+        eigvals = sla.eigsh(L, k=6, sigma=1e-8, which="LM", return_eigenvectors=False)
         eigvals = np.sort(eigvals)
         alg_connectivity = float(eigvals[1])
         spectral_gap = float(eigvals[2] - eigvals[1])
