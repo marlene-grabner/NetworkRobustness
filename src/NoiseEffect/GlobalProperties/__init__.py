1993dd3d21df8c1c0ef98ebc986532631421a461
marlene-grabner <marlene.grabner@gmail.com>
2026-09-04 10:44:46 +0200

    fix(GlobalProperties): add import statement

--- src/NoiseEffect/GlobalProperties/__init__.py
+++ src/NoiseEffect/GlobalProperties/__init__.py
@@ -1,2 +1,8 @@
 from .fiedler_value import fiedler_on_gcc
 from .calculate_gcc_singletons import calculate_singletons_and_gcc, _process_singletons_and_gcc
+from .global_efficiency import (
+    load_baseline_node_index,
+    build_graph,
+    global_efficiency,
+    global_efficiency_from_edges,
+)
